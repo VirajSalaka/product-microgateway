4731fba16522238e181d74615b4e7cffe7267853
VirajSalaka <viraj.14@cse.mrt.ac.lk>
2019-06-28 17:55:25 +0530

    add micro-gw as home

--- tests/src/test/java/org/wso2/micro/gateway/tests/common/CLIExecutor.java
+++ tests/src/test/java/org/wso2/micro/gateway/tests/common/CLIExecutor.java
@@ -76,7 +76,7 @@ public class CLIExecutor {
      */
     private void runProcess(String[] cmdArray, String homeDirectory, String errorMessage) throws MicroGWTestException {
         try {
-            Process process = Runtime.getRuntime().exec(cmdArray, null, new File(homeDirectory));
+            Process process = Runtime.getRuntime().exec(cmdArray, new String[] {"MICROGW_HOME=" + cliHome}, new File(homeDirectory));
             new ServerLogReader("errorStream", process.getErrorStream()).start();
             new ServerLogReader("inputStream", process.getInputStream()).start();
             boolean isCompleted = process.waitFor(2, TimeUnit.MINUTES);
